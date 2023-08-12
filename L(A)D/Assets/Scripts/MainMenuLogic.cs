299daca5946087e48aae70664a2a3969dcf9b9ba
Spearton <spearton11@gmail.com>
2023-08-12 13:00:34 +0200

    хуй

---
+++ L(A)D/Assets/Scripts/MainMenuLogic.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    [SerializeField]
+    private Camera Camera;
+    [SerializeField]
+    private GameObject NewGameMenu;
+    [SerializeField]
+    private GameObject SettingsMenu;
+    [SerializeField]
+    private GameObject MainMenu;
+    [SerializeField]
+    private GameObject Animation;
+    [SerializeField]
+    private GameObject NewGameMenuLight;
+    [SerializeField]
+    private GameObject SettingsMenuLight;
+    [SerializeField]
+    private GameObject MainMenuLight;
+    [SerializeField]
+    private GameObject AnimationLight;
+    private void Start()
+    {
+        
+    }
+    public void OnNewGameClick()
+    {
+        MainMenu.SetActive(false);
+        //Анимация перехода
+        NewGameMenu.SetActive(true);
+    }
+    public void OnSettingsClick()
+    {
+        MainMenu.SetActive(false);
+        //Анимация перехода
+        SettingsMenu.SetActive(true);
+    }
+    public void OnExitNewGameClick()
+    {
+        NewGameMenu.SetActive(false);
+        //Анимация перехода
+        MainMenu.SetActive(true);
+    }
+    public void OnExitSettingsClick()
+    {
+        SettingsMenu.SetActive(false);
+        //Анимация перехода
+        MainMenu.SetActive(true);
+    }
+    public void OnExitClick() => Application.Quit();
+}
