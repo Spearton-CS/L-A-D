4dbc046eee0d7e8ea7466be7db16708311413361
Spearton <spearton11@gmail.com>
2023-08-13 22:55:13 +0200

    Поправка баZы #1

--- L(A)D/Assets/Scripts/Enemies/RangedCombatEnemy.cs
+++ L(A)D/Assets/Scripts/Enemies/RangedCombatEnemy.cs
@@ -5,6 +5,8 @@ namespace Enemies
     /// <summary>Base (NON-ABSTRACT!!!) class for enemy in ranged combat</summary>
     public class RangedCombatEnemy : Enemy
     {
+        [SerializeField]
+        private protected readonly GameObject MissileObject;
         private void Update()
         {
         
