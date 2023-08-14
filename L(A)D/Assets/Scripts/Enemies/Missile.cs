c282c6f060a9cbfc62d6fd211609c1caaad376cf
Spearton <spearton11@gmail.com>
2023-08-14 18:51:40 +0200

    ничего

--- L(A)D/Assets/Scripts/Enemies/Missile.cs
+++ L(A)D/Assets/Scripts/Enemies/Missile.cs
@@ -1,11 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Missile : Enemies.Enemy
 {
     /// <summary>if nearest than zero calculates range by object size</summary>
     [SerializeField]
     private float MaxFlightRange;
-    private float CalculateMaxFlightRange() => transform.localScale.x * transform.localScale.y * 0.1F; //Redact later...
+    private float CalculateMaxFlightRange() => transform.localScale.x * transform.localScale.y * 0.1F;
+    private Rigidbody2D Body;
+    private void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+    }
     public void Fire(GameObject to)
     {
         float range;
@@ -13,24 +20,14 @@ public class Missile : Enemies.Enemy
             range = CalculateMaxFlightRange();
         else
             range = MaxFlightRange;
-        //Redact later...
+        float h = Mathf.Sign(to.transform.position.x - transform.position.x), v = Mathf.Sign(to.transform.position.y - transform.position.y);
+
     }
     #region Objects
-    public static GameObject Fireball()
-    {
-        return default;
-    }
-    public static GameObject Arrow()
-    {
-        return default;
-    }
-    public static GameObject Energy()
-    {
-        return default;
-    }
-    public static GameObject Water()
-    {
-        return default;
-    }
+    private static GameObject Copy(string name) => Instantiate(SceneManager.GetActiveScene().GetRootGameObjects().Where((x) => x.name == name).First(), new Vector3(), new Quaternion(0, 0, 0, 0));
+    public static GameObject Fireball() => Copy("Fireball");
+    public static GameObject Arrow() => Copy("Arrow");
+    public static GameObject Energy() => Copy("Energy");
+    public static GameObject Water() => Copy("Water");
     #endregion
 }
